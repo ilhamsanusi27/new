e37150bc5900e4b7f05c5be1870a8313dbbd2eb4
Ilham Sanusi <34309870+ilhamsanusi27@users.noreply.github.com>
2020-12-19 11:00:12 +0700

    Update and rename search7.js to search8.js

--- search8.js
+++ search8.js
@@ -14,7 +14,7 @@ xmlhttp.onreadystatechange = function() {
             count = a[i].hashtag.media_count;
             title = a[i].hashtag.search_result_subtitle;
 
-            x += '<li><a class="-qQT3" href="/search/label/' + name + '"><div class=" Igw0E rBNOH eGOV_ ybXk5 _4EzTm XfCBB HVWg4 "><div class=" Igw0E IwRSH eGOV_ _4EzTm yC0tu "><div class="FuWoR j44N4 zlwcS " style="width: 44px; height: 44px;"><span aria-label="Tagar" class="glyphsSpriteHashtag__outline__24__grey_9 u-__7"></span></div></div><div class=" Igw0E IwRSH YBx95 vwCYk "><div class=" Igw0E IwRSH eGOV_ _4EzTm " id="f131bea878af53c"><div class="_7UhW9 xLCgt MMzan KV-D4 fDxYl "><div class="_7UhW9 xLCgt qyrsm KV-D4 uL8Hv ">#' + name + '</div></div></div><div class=" Igw0E IwRSH eGOV_ _4EzTm DhRcB " id="f1e944189ae015"><div class="_7UhW9 xLCgt MMzan _0PwGv fDxYl "><span class=" "><span class=" ">' + title + '</span> </span></div></div></div></div></a></li>';
+            x += '<li><a class="-qQT3" href="/search/label/' + name + '?label=' + name + '"><div class=" Igw0E rBNOH eGOV_ ybXk5 _4EzTm XfCBB HVWg4 "><div class=" Igw0E IwRSH eGOV_ _4EzTm yC0tu "><div class="FuWoR j44N4 zlwcS " style="width: 44px; height: 44px;"><span aria-label="Tagar" class="glyphsSpriteHashtag__outline__24__grey_9 u-__7"></span></div></div><div class=" Igw0E IwRSH YBx95 vwCYk "><div class=" Igw0E IwRSH eGOV_ _4EzTm " id="f131bea878af53c"><div class="_7UhW9 xLCgt MMzan KV-D4 fDxYl "><div class="_7UhW9 xLCgt qyrsm KV-D4 uL8Hv ">#' + name + '</div></div></div><div class=" Igw0E IwRSH eGOV_ _4EzTm DhRcB " id="f1e944189ae015"><div class="_7UhW9 xLCgt MMzan _0PwGv fDxYl "><span class=" "><span class=" ">' + title + '</span> </span></div></div></div></div></a></li>';
 
         }
 
