991efee2c0572c8f633d3958a327367395a2c282
Ilham Sanusi <34309870+ilhamsanusi27@users.noreply.github.com>
2020-12-19 10:34:03 +0700

    Update and rename search6.js to search7.js

--- search7.js
+++ search7.js
@@ -1,15 +1,3 @@
-function getQueryVariable(variable) {
-    var query = window.location.search.substring(1);
-    var vars = query.split("&");
-    for (var i = 0; i < vars.length; i++) {
-        var pair = vars[i].split("=");
-        if (pair[0] == variable) {
-            return pair[1];
-        }
-    }
-    return (false);
-}
-
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
