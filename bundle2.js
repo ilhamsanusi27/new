ace820a3137b05f752295c8b86e50658e01747cf
Ilham Sanusi <34309870+ilhamsanusi27@users.noreply.github.com>
2020-12-19 23:04:35 +0700

    Update and rename bundle1.js to bundle2.js

--- bundle2.js
+++ bundle2.js
@@ -17,7 +17,9 @@ if (v != "") { var vv = loadScript("https://cdn.statically.io/gh/ilhamsanusi27/n
 if (q != "") {
 var qq = loadScript("https://cdn.statically.io/gh/ilhamsanusi27/new/main/search11.js"); setTimeout(qq, 1000);
 } else { var ii = loadScript("https://cdn.statically.io/gh/ilhamsanusi27/new/main/index5.js"); setTimeout(ii, 2000); }
-
+function downloadJSAtOnload() {
+loadScript("https://cdn.statically.io/gh/ilhamsanusi27/new/main/analytics.js");
+}
 if (window.addEventListener) {
 window.addEventListener("load", downloadJSAtOnload, false); }
 else if (window.attachEvent) {
