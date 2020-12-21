7d8dec73f241b5b306ce65a799d0a1094172acb3
Ilham Sanusi <34309870+ilhamsanusi27@users.noreply.github.com>
2020-12-22 01:09:19 +0700

    Update and rename bundle5.js to bundle6.js

---
+++ bundle6.js
@@ -0,0 +1,21 @@
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
+    loadScript("https://cdn.statically.io/gh/ilhamsanusi27/new/main/root8.js");
+
+var v = getQueryVariable("v");
+var q = getQueryVariable("q");
+
+if (v != "") { setTimeout(loadScript("https://cdn.statically.io/gh/ilhamsanusi27/new/main/views5.js"), 1000); }
+if (q != "") {
+setTimeout(loadScript("https://cdn.statically.io/gh/ilhamsanusi27/new/main/search11.js"), 1000);
+} else { setTimeout(loadScript("https://cdn.statically.io/gh/ilhamsanusi27/new/main/index5.js"), 2000); }
+
+var lazy=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazy||0!=document.body.scrollTop&&!1===lazy)&&(!function(){loadScript("https://cdn.statically.io/gh/ilhamsanusi27/new/main/analytics4.js");}(),lazy=!0)},!0);
