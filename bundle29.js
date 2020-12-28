3c6b0ea1b796c145f79a4f80921b9d50c623b3e2
Ilham Sanusi <34309870+ilhamsanusi27@users.noreply.github.com>
2020-12-28 19:00:45 +0700

    Update and rename bundle28.js to bundle29.js

--- bundle29.js
+++ bundle29.js
@@ -8,24 +8,42 @@ function getQueryVariable(variable)
        }
        return(false);
 }
-    loadScript("https://cdn.statically.io/gh/ilhamsanusi27/new/main/root12.js");
+
+loadScript("https://cdn.statically.io/gh/ilhamsanusi27/new/main/root13.js");
+
+function shareOverrideOGMeta(overrideLink, overrideTitle, overrideDescription, overrideImage)
+{
+	FB.ui({
+		method: 'share_open_graph',
+		action_type: 'og.likes',
+		action_properties: JSON.stringify({
+			object: {
+				'og:url': overrideLink,
+				'og:title': overrideTitle,
+				'og:description': overrideDescription,
+				'og:image': overrideImage
+			}
+		})
+	},
+	function (response) {
+	// Action after response
+	});
+}
 
 var v = getQueryVariable("v");
 var q = getQueryVariable("q");
-var p1 = getQueryVariable("p1");
-var p2 = getQueryVariable("p2");
-var at = getQueryVariable("at");
 
-if (v != "") { setTimeout(loadScript("https://cdn.statically.io/gh/ilhamsanusi27/new/main/views10.js"), 1000); }
+if (v != "") { setTimeout(loadScript("https://cdn.statically.io/gh/ilhamsanusi27/new/main/views12.js"), 1000); }
 if (q != "") {
 setTimeout(loadScript("https://cdn.statically.io/gh/ilhamsanusi27/new/main/search11.js"), 1000);
-} else { setTimeout(loadScript("https://cdn.statically.io/gh/ilhamsanusi27/new/main/index16.js"), 2000); }
+} else { setTimeout(loadScript("https://cdn.statically.io/gh/ilhamsanusi27/new/main/index17.js"), 2000); }
 
 var lazy=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazy||0!=document.body.scrollTop&&!1===lazy)&&(!function(){
 
 //loadScript("https://cdn.statically.io/gh/ilhamsanusi27/new/main/analytics5.js");
 
   //facebook pixel
+if(px == "yes"){
   !function(f,b,e,v,n,t,s)
   {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
   n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -37,7 +55,10 @@ var lazy=!1;window.addEventListener("scroll",function(){(0!=document.documentEle
   fbq('init', '2283154871992432');
   fbq('track', 'PageView');
   
+}
+
   //facebook js
+if(fb == "yes"){
   window.fbAsyncInit = function() {
     FB.init({
       appId      : '2512602489066986',
@@ -62,51 +83,35 @@ var lazy=!1;window.addEventListener("scroll",function(){(0!=document.documentEle
      js.src = "https://connect.facebook.net/en_US/sdk.js";
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
+}
 
  //Google Tag Manager
+if(gtm == "yes"){
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-NZDNTZZ');
-
+}
   //google analytics
+if(ga == "yes"){
 loadScript("https://www.googletagmanager.com/gtag/js?id=UA-121877700-7","","","async");
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
   gtag('config', 'UA-121877700-7');
-
+}
 //propeller
 //notif
-if (p1 != "") {
+if (p1 == "yes") {
 loadScript("https://choupsee.com/pfe/current/tag.min.js?z=3796448","data-cfasync","false","async");}
 
 //Multitag
-if (p2 != "") {
+if (p2 == "yes") {
 setTimeout(loadScript("https://iclickcdn.com/tag.min.js","data-zone","3797617"), 60000); }
 
-if (at != "") {
+if (at == "yes") {
 loadScript("//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-58a25e0fe19119b4","","","","type");      document.getElementById("at").style.height = "80px";
       }
 
 }(),lazy=!0)},!0);
-
-function shareOverrideOGMeta(overrideLink, overrideTitle, overrideDescription, overrideImage)
-{
-	FB.ui({
-		method: 'share_open_graph',
-		action_type: 'og.likes',
-		action_properties: JSON.stringify({
-			object: {
-				'og:url': overrideLink,
-				'og:title': overrideTitle,
-				'og:description': overrideDescription,
-				'og:image': overrideImage
-			}
-		})
-	},
-	function (response) {
-	// Action after response
-	});
-}
