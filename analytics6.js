5630105ca597efe62a64539dc30a62d640da002a
Ilham Sanusi <34309870+ilhamsanusi27@users.noreply.github.com>
2020-12-28 22:11:30 +0700

    Update and rename analytics5.js to analytics6.js

--- analytics6.js
+++ analytics6.js
@@ -1,4 +1,5 @@
   //facebook pixel
+if(px == "yes"){
   !function(f,b,e,v,n,t,s)
   {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
   n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -10,7 +11,10 @@
   fbq('init', '2283154871992432');
   fbq('track', 'PageView');
   
+}
+
   //facebook js
+if(fb == "yes"){
   window.fbAsyncInit = function() {
     FB.init({
       appId      : '2512602489066986',
@@ -29,33 +33,39 @@
 }
 
   (function(d, s, id){
-     var js, js2, fjs = d.getElementsByTagName(s)[0];
+     var js, fjs = d.getElementsByTagName(s)[0];
      if (d.getElementById(id)) {return;}
      js = d.createElement(s); js.id = id;
      js.src = "https://connect.facebook.net/en_US/sdk.js";
      fjs.parentNode.insertBefore(js, fjs);
-        js2 = d.createElement(s); js2.id = id;
-        js2.src = 'https://connect.facebook.net/id_ID/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js2, fjs);
    }(document, 'script', 'facebook-jssdk'));
-  
+}
+
  //Google Tag Manager
+if(gtm == "yes"){
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-NZDNTZZ');
-
+}
   //google analytics
+if(ga == "yes"){
 loadScript("https://www.googletagmanager.com/gtag/js?id=UA-121877700-7","","","async");
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
   gtag('config', 'UA-121877700-7');
-
+}
 //propeller
 //notif
-loadScript("https://choupsee.com/pfe/current/tag.min.js?z=3796448","data-cfasync","false","async");
+if (p1 == "yes") {
+loadScript("https://choupsee.com/pfe/current/tag.min.js?z=3796448","data-cfasync","false","async");}
 
 //Multitag
-setTimeout(loadScript("https://iclickcdn.com/tag.min.js","data-zone","3797617"), 60000);
+if (p2 == "yes") {
+setTimeout(loadScript("https://iclickcdn.com/tag.min.js","data-zone","3797617"), 60000); }
+
+if (at == "yes") {
+loadScript("//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-58a25e0fe19119b4","","","","type");      document.getElementById("at").style.height = "80px";
+      }
