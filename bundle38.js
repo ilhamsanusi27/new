e682b6fc20da3226bd83a1b6bbb857cd1e83ed6f
Ilham Sanusi <34309870+ilhamsanusi27@users.noreply.github.com>
2020-12-30 05:52:53 +0700

    Update and rename bundle37.js to bundle38.js

--- bundle38.js
+++ bundle38.js
@@ -9,7 +9,7 @@ function getQueryVariable(variable)
        return(false);
 }
 
-loadScript("https://cdn.statically.io/gh/ilhamsanusi27/new/main/root16.js");
+loadScript("https://cdn.statically.io/gh/ilhamsanusi27/new/main/root17.js");
 
 var q = getQueryVariable("q");
 
