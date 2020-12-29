7fc81765356121c79ed735a0e3f4d9205122b869
Ilham Sanusi <34309870+ilhamsanusi27@users.noreply.github.com>
2020-12-29 22:33:45 +0700

    Update bundle33.js

--- bundle33.js
+++ bundle33.js
@@ -9,12 +9,12 @@ function getQueryVariable(variable)
        return(false);
 }
 
-loadScript("https://cdn.statically.io/gh/ilhamsanusi27/new/main/root13.js");
+loadScript("https://cdn.statically.io/gh/ilhamsanusi27/new/main/root14.js");
 
 var q = getQueryVariable("q");
 
 if (q != "") {
 setTimeout(loadScript("https://cdn.statically.io/gh/ilhamsanusi27/new/main/search11.js"), 1000);
-} else { setTimeout(loadScript("https://cdn.statically.io/gh/ilhamsanusi27/new/main/index20.js"), 2000); }
+} else { setTimeout(loadScript("https://cdn.statically.io/gh/ilhamsanusi27/new/main/index21.js"), 2000); }
 
 var lazy=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazy||0!=document.body.scrollTop&&!1===lazy)&&(!function(){loadScript("https://cdn.statically.io/gh/ilhamsanusi27/new/main/analytics6.js");}(),lazy=!0)},!0);
