cacfe74619b089cf9cd4554537c484d9797b8a82
Ilham Sanusi <34309870+ilhamsanusi27@users.noreply.github.com>
2020-12-31 00:02:36 +0700

    Update and rename bundle41.js to bundle42.js

--- bundle42.js
+++ bundle42.js
@@ -9,7 +9,7 @@ function getQueryVariable(variable)
        return(false);
 }
 
-loadScript("https://cdn.statically.io/gh/ilhamsanusi27/new/main/root20.js");
+loadScript("https://cdn.statically.io/gh/ilhamsanusi27/new/main/root21.js");
 
 var q = getQueryVariable("q");
 
