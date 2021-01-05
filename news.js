68cb5932379af28a7e1d8fd28d4611e311580e30
Ilham Sanusi <34309870+ilhamsanusi27@users.noreply.github.com>
2021-01-05 18:53:27 +0700

    Update and rename news.js to news1.js

--- news.js
+++
@@ -1,5 +0,0 @@
-loadScript("https://cdn.statically.io/gh/ilhamsanusi27/new/main/root1.js");
-
-loadScript("https://platform-api.sharethis.com/js/sharethis.js#property=5fe0e8333a4bba00125c6c97&product=sop","","","async","type");
-
-var lazy=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazy||0!=document.body.scrollTop&&!1===lazy)&&(!function(){loadScript("https://cdn.statically.io/gh/ilhamsanusi27/new/main/analytics.js"); }(),lazy=!0)},!0);
